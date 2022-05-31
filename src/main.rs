c02ed84347dfca41304559b4d19b4c58aa32b087
dhzdhd <ragas5432100@gmail.com>
2022-05-31 23:06:10 +0530

    Add stuff

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use cursive::{self, views::{TextView, Dialog, LinearLayout, Button, PaddedView}, Cursive, reexports::time::format_description::modifier::Padding, traits::Nameable};
+use cursive::{self, views::{Dialog, LinearLayout, Button, PaddedView, NamedView}, Cursive, view::{Nameable}};
+
+struct Row(String, String, String);
 
 fn main() {
     let mut siv = cursive::default();
@@ -14,9 +16,21 @@ fn main() {
 }
 
 fn game_view(s: &mut Cursive) {
-    let mut is_x = false;
+    let mut ttt_vec = [""].repeat(9);
 
     s.pop_layer();
+
+    let mut body = LinearLayout::vertical();
+    for i in 0..3 {
+        let mut hort_layout = LinearLayout::horizontal();
+        body.add_child(hort_layout);
+
+        for j in 0..3 {
+            // FIXME: :)))))))))))
+            hort_layout.add_child(button(ttt_vec, format!("{}", i * j).as_str()))
+        }
+    }
+
     s.add_layer(
         PaddedView::lrtb(
             10,
@@ -25,6 +39,7 @@ fn game_view(s: &mut Cursive) {
             5,
             LinearLayout::vertical()
                 .child(LinearLayout::horizontal()
+
                     .child(Button::new(" ", |s| s.call_on_name("1", |button: &mut Button| button.set_label("X")).unwrap()).with_name("1"))
                     .child(Button::new(" ", |s| ()))
                     .child(Button::new(" ", |s| ()))
@@ -36,22 +51,21 @@ fn game_view(s: &mut Cursive) {
                 )
                 .child(LinearLayout::horizontal()
                     .child(Button::new(" ", |s| ()))
-                    .child(Button::new(" ", |s| ()))
-                    .child(button(is_x))
+
                 )
         )
     );
 }
 
-fn button(is_x: bool) -> Button {
-    Button::new(" ", |s: &mut Cursive| {
-        s.call_on_name("", |button: &mut Button| {
+fn button(vec: Vec<&'static str>, label: &'static str) -> NamedView<Button> {
+    return Button::new(" ", |s: &mut Cursive| {
+        s.call_on_name(label, |button: &mut Button| { //FIXME:
             button.set_label(
-                match is_x {
+                match vec.into_iter().filter(|e| *e != "").count() % 2 == 0 {
                     true => "x",
                     false => "o"
                 }
             );
         }).unwrap()
-    })
+    }).with_name(label);
 }
